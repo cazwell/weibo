8b12e738ee627a444faf6032dbf951db13adf609
cazwell <553227465@qq.com>
2022-05-28 09:49:26 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
+const mix = require('laravel-mix');
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+  .sass('resources/sass/app.scss', 'public/css').version();
